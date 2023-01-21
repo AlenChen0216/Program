8bf72aaa1a031cf90eb47d588686de5968b0a0e8
AlenChen0216 <alen0216@gmail,com>
2023-01-21 10:29:18 +0800

    Up

---
+++ Test/Untitled3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+   int b[11]={},n=0;
+   int i;
+      scanf("%d",&n);
+      for(i=1;i<=n;i++){
+      scanf("%d",&b[i]);
+	  }
+    for(i=n;i>=1;i--){
+    printf("%d ",&b[i]);
+	}
+   return 0;
+}
